c831f5d51f963e0741d49921295ca693a0330f11
Sergey Psekha <pss@ciklum.com>
2012-09-19 11:58:10 +0300

    develop commit

---
+++ Static/Scripts/Cockpit.Namespace.js
@@ -0,0 +1,14 @@
+function namespace(namespaceString) {
+	/* develop commit */
+  var parts = namespaceString.split('.'), 
+    parent = window, 
+    currentPart = '';
+  
+  for(var i = 0, length = parts.length; i < length; i++) {
+    currentPart = parts[i];
+    parent[currentPart] = parent[currentPart] || {};
+    parent = parent[currentPart];
+  }
+  
+  return parent;
+};
